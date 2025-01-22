66584900de7241ad75d5b496cb69ff04631e18eb
Samjanity996 <sam9960628@gmail.com>
2025-01-22 18:43:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 
 
 # Function to load the response from the saved file
-def load_response_from_file(filename="response.json"):
+def load_response_from_file(filename="./results/llm_result_5.json"):
     with open(filename, 'r') as f:
         data = json.load(f)
     return data
@@ -31,12 +31,15 @@ if prompt := st.chat_input("What is up?"):
     # Load the saved response from the JSON file
     response_data = load_response_from_file()  # Load the saved response
 
-    # Extract the assistant's response
-    assistant_response = response_data.get("assistant_response", "Sorry, I couldn't find a response.")
+    # Extract the assistant's response (first element of the list)
+    if response_data and isinstance(response_data, list):
+        generated_text = response_data[0].get("response")
+    else:
+        generated_text = "Sorry, I couldn't find a response."
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        st.markdown(assistant_response)
+        st.markdown(generated_text)
 
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": assistant_response})
+    st.session_state.messages.append({"role": "assistant", "content": generated_text})
